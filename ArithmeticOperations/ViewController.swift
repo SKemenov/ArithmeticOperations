7400c064966397db56cf8e457a021383fe2836ea
Sergey Kemenov <skemenov@icloud.com>
2023-06-13 21:22:38 +0300

    Initial Commit

---
+++ ArithmeticOperations/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ArithmeticOperations
+//
+//  Created by Sergey Kemenov on 13.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
